ed5440856fe9f3f1b002ce9845de626cd1bd3b9f
Vadim Mezentsev <mezentsevvo@gmail.com>
2022-04-21 11:24:55 +0300

    Added launch types for activity

--- app/src/main/java/com/example/basecomponents/MainActivity.kt
+++ app/src/main/java/com/example/basecomponents/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.example.basecomponents
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.basecomponents.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.startActivityButton.setOnClickListener {
+            startActivity(
+                Intent(
+                    this,
+                    MainActivity::class.java
+                ).apply {
+                    putExtra("EXTRA", "Hi there")
+
+                    /**
+                     * Testing with Launch types
+                     */
+                    addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+                }
+            )
+        }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        Toast.makeText(this, "${intent?.extras?.get("EXTRA")}", Toast.LENGTH_SHORT).show()
     }
 }
